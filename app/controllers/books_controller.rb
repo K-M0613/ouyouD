beebf441817ef8b8fbeb8ea543362d7819f83e0e
K-M0613 <yishumenma245@gmail.com>
2022-07-08 09:50:53 +0000

    8d

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,11 +13,20 @@ class BooksController < ApplicationController
   def index
     to  = Time.current.at_beginning_of_day
     from  = (to - 6.day).at_end_of_day
-    @books = Book.all.sort {|a,b| 
-      b.favorites.where(created_at: from...to).size <=> 
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
       a.favorites.where(created_at: from...to).size
     }
     @book = Book.new
+    if params[:latest]
+      @books = Book.latest
+    elsif params[:old]
+      @books = Book.old
+    elsif params[:star_count]
+      @books = Book.star_count
+    else
+      @books = Book.all
+    end
   end
 
   def create
@@ -29,6 +38,7 @@ class BooksController < ApplicationController
       @books = Book.all
       render 'index'
     end
+
   end
 
   def edit
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,16 +3,20 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :view_counts, dependent: :destroy
-  
+
   has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
+  scope :latest, -> {order(created_at: :desc)}
+  scope :old, -> {order(created_at: :asc)}
+  scope :star_count, -> {order(star: :desc)}
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-	
+
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
