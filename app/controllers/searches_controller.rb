962b0c514bf4d9755ae32cb2e1f717dae50e2574
K-M0613 <yishumenma245@gmail.com>
2022-07-09 08:46:22 +0000

    ouyou9d

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,13 +2,16 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
 	def search
+		@search_word = params[:search_word]
 		@model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
 		if @model == 'user'
 			@records = User.search_for(@content, @method)
-		else
+		elsif @model == 'book'
 			@records = Book.search_for(@content, @method)
+		else
+			@records = Book.search_for(@search_word, @method)
 		end
 	end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,8 +5,8 @@ class Book < ApplicationRecord
   has_many :view_counts, dependent: :destroy
 
   has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
-  
-  
+
+
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
@@ -30,8 +30,24 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', '%'+content+'%')
     end
   end
-  
-  def self.search(search_word)
-    Book.where(['category LIKE ?', "#{search_word}"])
+
+  def self.search_for(search_word,method)
+    if method == 'perfect'
+     Book.where(['category LIKE ?', "#{search_word}"])
+    end
   end
+
+  # def save_tags(savebook_tags)
+  #   current_tags = self.tags.pluck(:name) unless self.tags.nil?
+  #   old_tags = current_tags - savebook_tags
+  #   new_tags = savebook_tags - current_tags
+  #   old_tags.each do |old_name|
+  #     self.tags.delete Tag.find_by(name:old_name)
+  #   end
+
+  #   new_tags.each do |new_name|
+  #     book_tag = Tag.find_or_create_by(name:new_name)
+  #     self.tags << book_tag
+  #   end
+  # end
 end
